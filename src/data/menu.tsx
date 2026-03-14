7dbb13b01f4f1939a5f684d5630b4977fb318bab
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2026-03-14 10:14:55 +0545

    cleanup

--- src/data/menu.tsx
+++ src/data/menu.tsx
@@ -1,4 +1,4 @@
-import { externalUrls, mainSiteUrls, docUrls } from '@/consts'
+import { externalUrls, mainSiteUrls, docUrls } from '../consts'
 
 export type PopupName = 'dataVersionControlPopup' | 'communityPopup'
 
